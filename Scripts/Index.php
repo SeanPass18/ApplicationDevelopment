d78e38312c375ddee5fad9337ad8e926a970cd76
Sean <73589260+SeanPass18@users.noreply.github.com>
2023-02-07 13:57:35 +0000

    Session 1: Established 3 Pages, 3 reusable Partials and a CSS file

---
+++ Scripts/Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> 100512715 - Index</title>
+    <html-include>./partials/head.html</html-include>
+</head>
+<body>
+<html-include>./partials/nav.html</html-include>
+<h1>100512715 - Home</h1>
+<h2>Your center for data visualisation</h2>
+<p> How about you take a peek? :</p>
+<?php
+echo "Showing your Data!";
+?>
+<html-include>./partials/footer.html</html-include>
+</body>
+</html>
